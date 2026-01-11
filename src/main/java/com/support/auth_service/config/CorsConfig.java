eb9b238878d137dd4013da48d355dea390bf22d4
Punarvasu20003 <punarvasuingole72@gmail.com>
2026-01-11 15:14:09 +0530

    added the deployed frontend url to the cors configuration

--- src/main/java/com/support/auth_service/config/CorsConfig.java
+++ src/main/java/com/support/auth_service/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","https://support-frontend-six.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
